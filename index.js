3855bb9e50701210a1c04c82276e6c4c7c1babbb
gregconvery <gregconvery@gmail.com>
2020-05-18 20:14:15 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
+const server = express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/Table4'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  socket.on('PlayerSit', (msg) => {
+    io.emit('PlayerSit', msg);
+  });
+});
